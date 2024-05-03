b67cbb1c9ae8f4b3706eacd76f024c3fcce687d1
Martinko1006 <116968592+Martinko1006@users.noreply.github.com>
2024-05-03 19:12:59 +0200

    Add files via upload

--- 404.js
+++ 404.js
@@ -1,3 +1,7 @@
 document.getElementById('go-back').onclick = function() {
     window.location = 'index.html'
 }
+
+document.getElementById('download-button').onclick = function() {
+    alert("The mCalc application is not anivable at this time")
+}
